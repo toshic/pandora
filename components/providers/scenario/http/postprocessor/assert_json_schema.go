78178e5cd78d81462b60b56a76466a025a4e2fba
Dmitry Sagan <sagandmitry03@gmail.com>
2024-08-31 10:51:38 +0700

    fix issues after testing, update docs

--- components/providers/scenario/http/postprocessor/assert_json_schema.go
+++ components/providers/scenario/http/postprocessor/assert_json_schema.go
@@ -5,20 +5,58 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 	"io"
 	"net/http"
+	"path/filepath"
 	"strings"
 )
 
 type AssertJsonSchema struct {
-	SchemaPath string `config:"schema_path"`
+	Source string `config:"source"`
+	Schema string `config:"schema"`
 }
 
-func (a AssertJsonSchema) Process(resp *http.Response, _ io.Reader) (map[string]any, error) {
-	schema := gojsonschema.NewReferenceLoader(a.SchemaPath)
-	respJson, _ := gojsonschema.NewReaderLoader(resp.Body)
-	result, err := gojsonschema.Validate(schema, respJson)
+func (a AssertJsonSchema) Process(_ *http.Response, body io.Reader) (map[string]any, error) {
+	var schemaLoader gojsonschema.JSONLoader
+
+	switch a.Source {
+	case "file":
+		absPath, err := filepath.Abs(a.Schema)
+
+		if err != nil {
+			return nil, fmt.Errorf("assert/json-schema fail to get schema file absolute path: %w", err)
+		}
+
+		filePath := "file:///" + absPath
+		schemaLoader = gojsonschema.NewReferenceLoader(filePath)
+
+	case "url":
+		schemaLoader = gojsonschema.NewReferenceLoader(a.Schema)
+	case "json_string":
+		schemaLoader = gojsonschema.NewStringLoader(a.Schema)
+	default:
+		return nil, fmt.Errorf("unknown schema source %s", a.Source)
+	}
+
+	var b []byte
+	var err error
+
+	if body != nil {
+		b, err = io.ReadAll(body)
+		if err != nil {
+			return nil, fmt.Errorf("assert/json-schema can't read body: %w", err)
+		}
+	}
+
+	schema, err := gojsonschema.NewSchema(schemaLoader)
 
 	if err != nil {
-		return nil, fmt.Errorf("assert/json-schema check error: %w", err)
+		return nil, fmt.Errorf("assert/json-schema can't create schema: %w", err)
+	}
+
+	respJson := gojsonschema.NewBytesLoader(b)
+	result, err := schema.Validate(respJson)
+
+	if err != nil {
+		return nil, fmt.Errorf("assert/json-schema validate error: %w", err)
 	}
 
 	if !result.Valid() {
@@ -35,8 +73,23 @@ func (a AssertJsonSchema) Process(resp *http.Response, _ io.Reader) (map[string]
 
 func (a AssertJsonSchema) Validate() error {
 
-	if a.SchemaPath == "" {
-		return fmt.Errorf("assert/json-schema validation json-schema not found in path %s", a.SchemaPath)
+	if a.Source != "file" && a.Source != "url" && a.Source != "json_string" {
+		return fmt.Errorf("assert/json-schema unknown schema source %s", a.Source)
+	}
+
+	if a.Schema == "" {
+		var invalidSource string
+
+		switch a.Source {
+		case "file":
+			invalidSource = "file path"
+		case "url":
+			invalidSource = "url"
+		case "json_string":
+			invalidSource = "json string"
+		}
+
+		return fmt.Errorf("assert/json-schema invalid schema %s", invalidSource)
 	}
 
 	return nil
